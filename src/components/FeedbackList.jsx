6900dabe1763b84564b00a8ec8e039b8702e1272
Gokul Pandian C <gokulcofficial@gmail.com>
2022-12-15 23:39:25 +0530

    initial

---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,24 @@
+import FeedbackItem from "./FeedbackItem";
+import { useContext } from "react";
+import FeedbackContext from "../context/FeedbackContext";
+
+function FeedbackList() {
+
+  const { feedback } = useContext(FeedbackContext);
+  if (!feedback || feedback.length === 0) {
+    return <p>No Feedback yet</p>;
+  }
+  return (
+    <div>
+      {feedback.map((item) => (
+        <FeedbackItem
+          key={item.id}
+          item={item}
+        />
+      ))}
+    </div>
+  );
+}
+
+
+export default FeedbackList;
